d2f55019018418587fa7e87f1cfd8279c5810ec6
unknown <sanya@example.com>
2025-03-29 23:27:36 +0700

    add_5

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import product_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/',home, name='home'),
-    path('contacts/',contacts, name='contacts')
+    path('', product_list, name='product_list'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from django.conf.global_settings import STATICFILES_DIRS
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT, MEDIA_URL
 import os
 from dotenv import load_dotenv
 
@@ -133,3 +133,7 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
