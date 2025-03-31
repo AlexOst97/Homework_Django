57f75ed298e33fe25a01e00781b5ca6d888009ee
unknown <sanya@example.com>
2025-03-31 21:55:48 +0700

    add_6

--- catalog/views.py
+++ catalog/views.py
@@ -5,3 +5,8 @@ def product_list(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'product_list.html', context)
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, 'product_info.html', context)
